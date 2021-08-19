ba1a2517098247e18f072a513ebf322258e0cfa2
96sMicks <melkiasae@hotmail.com>
2021-08-19 03:41:04 +0000

    A code to tell at what indexes are letters located

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const assertArraysEqual = function(arr, arr2) {
+  if (arr.length !== arr2.length) {
+    console.log("🛑These arrays don't match in length");
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr2[i]) {
+      console.log("🛑These arrays do not match");
+      return false;       
+    }
+  }
+  console.log("✅These arrays match");
+  return true;
+};
+
+const eqArrays = function(arr, arr2) {
+  if (arr.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr2[i]) {
+      return false;       
+    }
+  } 
+  return true;
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      }  else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
