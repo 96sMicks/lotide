4da76f162b507f6bd83b579575d810dfe678d4eb
96sMicks <melkiasae@hotmail.com>
2021-08-23 17:46:58 +0000

    refactored our test code

---
+++ test/assertEqual.Test.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual')
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("yo", "yo");
+assertEqual(12,7);
+assertEqual([1, 2], [1, 2]);
